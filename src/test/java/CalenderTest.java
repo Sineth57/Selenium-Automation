e376a5dc49f04a7f77057437c1becd552c2d5478
Sineth Shashintha <sinethshashintha2000@gmail.com>
2026-06-17 22:06:25 +0530

    calender data picker reuserble method

--- src/test/java/CalenderTest.java
+++ src/test/java/CalenderTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class CalenderTest {
 
     WebDriver driver;
@@ -41,11 +43,14 @@ public class CalenderTest {
 
         driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
 
+        WebElement datePicker = driver.findElement(By.xpath("//input[@id='datepicker']"));
+        datePicker.click();
+
         while (true){
             String actualYear = driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
             String actualMonth = driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
 
-            if (actualYear.equals(2026) && actualMonth.equals("June")){
+            if (actualYear.equals("2026") && actualMonth.equals("October")){
                 break;
             }else {
                 driver.findElement(By.xpath("//a[@title='Next']")).click();
@@ -54,6 +59,46 @@ public class CalenderTest {
         }
 
 
+        List<WebElement> allDates = driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']/tbody/tr/td/a"));
+
+        for (WebElement dateElement : allDates){
+            if (dateElement.getText().equals("3")){
+                dateElement.click();
+                break;
+            }
+        }
+    }
+
+
+    // if we want to reuse this in another places we can create a method to use it whenever we want
+
+    public void selectDate(String year, String month, String date){
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+
+        WebElement datePicker = driver.findElement(By.xpath("//input[@id='datepicker']"));
+        datePicker.click();
+
+        while (true){
+            String actualYear = driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
+            String actualMonth = driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
+
+            if (actualYear.equals(year) && actualMonth.equals(month)){
+                break;
+            }else {
+                driver.findElement(By.xpath("//a[@title='Next']")).click();
+            }
+
+        }
+
+
+        List<WebElement> allDates = driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']/tbody/tr/td/a"));
+
+        for (WebElement dateElement : allDates){
+            if (dateElement.getText().equals(date)){
+                dateElement.click();
+                break;
+            }
+        }
     }
 
 }
